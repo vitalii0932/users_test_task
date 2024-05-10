2385d63e0dfe9c4adbdc8ae7395a3c11be8dce17
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-10 16:26:55 +0300

    user service

--- src/main/java/com/example/users_test_task/service/UserService.java
+++ src/main/java/com/example/users_test_task/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.users_test_task.service;
 
+import com.example.users_test_task.model.User;
 import com.example.users_test_task.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,5 +14,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * save user in db
+     *
+     * @param user - user model
+     * @return - saved user entity
+     */
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
