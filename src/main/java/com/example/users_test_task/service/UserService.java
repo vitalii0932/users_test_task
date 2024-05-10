00f8ba6191611d0789d1109f6cf7c1cd31071d64
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-10 16:18:39 +0300

    init user repo

---
+++ src/main/java/com/example/users_test_task/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.users_test_task.service;
+
+import com.example.users_test_task.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * service for working with user entities and db
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
