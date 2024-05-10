566ac5a91e92dfbc35b3ec80cae5e5dae01fc905
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-10 16:05:55 +0300

    create user model

---
+++ src/main/java/com/example/users_test_task/model/User.java
@@ -0,0 +1,43 @@
+package com.example.users_test_task.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+
+import java.util.Date;
+
+/**
+ * user entity class
+ */
+@Data
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotEmpty(message = "First name is required")
+    private String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    private String lastName;
+
+    @Past(message = "Date of birth should be in the past")
+    private Date dateOfBirth;
+
+    private String address;
+
+    @Pattern(regexp="[0-9]+", message="Phone number should contain only digits")
+    private String phoneNumber;
+}
